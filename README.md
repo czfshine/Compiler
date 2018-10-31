8a3cb7d83a8439c8eb70ea8a7285a2cbe1b04573
chen zefan <czfshine@outlook.com>
2018-10-31 20:01:53 +0800

    很久之前的代码了

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Compiler
 &lt;Compiler Principles ,Techniques,&amp;Tools 2nd> exercise codes.&lt;编译原理>练习代码
+
+**我的一些关于编译器前端后端的代码,小片段,例子,文章和推导.**
+
+
+
+
+
---
+++ SimpleParser/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace SimpleParser
+{
+    /// <summary>
+    /// 一个简单的递归下降解析器
+    /// </summary>
+    class SimpleParser
+    {
+        static void Main(string[] args)
+        {
+            String code = Console.ReadLine();
+            new SimpleParser().Parser(code);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        char[] chars;
+        int i = 0;
+        char NextChar()
+        {
+            if (i < chars.Length)
+            {
+                return chars[i++];
+
+            }
+            else
+            {
+                return '\0';
+            }
+        }
+        char curchar;
+        void Parser(String code)
+        {
+            chars= code.ToCharArray();
+            curchar = NextChar();
+            expr();
+        }
+
+        void expr()
+        {
+            term();
+            while (true)
+            {
+                if (curchar == '+')
+                {
+                    match('+');
+                    term();
+                    Console.Write('+');
+                }
+                else if (curchar == '-')
+                {
+                    match('-');
+                    term();
+                    Console.Write('-');
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+        void term()
+        {
+            if (Char.IsDigit(curchar))
+            {
+                Console.Write(curchar);
+                match(curchar);
+            }
+            else
+            {
+                Console.WriteLine("term error");
+                return;
+            }
+        }
+        void match(char c)
+        {
+            if (curchar == c)
+            {
+                curchar = NextChar();
+            }
+            else
+            {
+                Console.WriteLine("match error");
+            }
+        }
+    }
+}
